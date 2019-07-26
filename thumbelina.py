866f56e3470a4aa335c9e7a04b905a29a5cdd199
Bernard Duggan <bernard@hippware.com>
2019-07-26 11:23:32 +1000

    Add aspect-correct thumbnail

--- thumbelina.py
+++ thumbelina.py
@@ -10,8 +10,14 @@ from subprocess import Popen, PIPE
 s3 = boto3.client('s3')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+targetBucket = None
+body = None
+response = None
+contentType = None
 
 def lambda_handler(event, context):
+    global targetBucket, body, response, contentType
+
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
 
@@ -37,11 +43,7 @@ def lambda_handler(event, context):
         cleanImage = p.communicate(input=body)[0]
 
         logger.info('Uploading clean image')
-        s3.put_object(Bucket = targetBucket,
-                      Key = key,
-                      Body = cleanImage,
-                      Metadata = response['Metadata'],
-                      ContentType = contentType)
+        upload_to_s3(key, cleanImage)
 
         # Make thumbnail
         logger.info('Creating thumbnail')
@@ -50,20 +52,21 @@ def lambda_handler(event, context):
         thumbnailImage = p.communicate(input=body)[0]
 
         logger.info('Uploading thumbnail')
-        s3.put_object(Bucket = targetBucket,
-                      Key = key + "-thumbnail",
-                      Body = thumbnailImage,
-                      Metadata = response['Metadata'],
-                      ContentType = contentType)
+        upload_to_s3(key + "-thumbnail", thumbnailImage)
+
+        # Make aspect-ratio-correct thumbnail
+        logger.info('Creating aspect-ratio-correct thumbnail')
+        cmd = cmdHead + aspect_thumbnail_params() + cmdTail
+        p = Popen(cmd, stdout=PIPE, stdin=PIPE)
+        aspectThumbnailImage = p.communicate(input=body)[0]
+
+        logger.info('Uploading aspect-ratio-correct thumbnail')
+        upload_to_s3(key + "-aspect_thumbnail", aspectThumbnailImage)
 
         # Copy over the original image (in case we want to reprocess it in the
         # future)
         logger.info('Uploading original')
-        s3.put_object(Bucket = targetBucket,
-                      Key = key + "-original",
-                      Body = body,
-                      Metadata = response['Metadata'],
-                      ContentType = contentType)
+        upload_to_s3(key + "-original", body)
 
         # Update the database to mark the image as processed
         mark_processed(key)
@@ -99,6 +102,17 @@ def thumbnail_params():
             '-gravity', 'center',
             '-extent', thumb_size]
 
+def aspect_thumbnail_params():
+    # As for thumbnails, but maintain aspect ratio and do not crop
+    return ['-thumbnail', "360x360"]
+
+def upload_to_s3(key, body):
+    s3.put_object(Bucket = targetBucket,
+                  Key = key,
+                  Body = body,
+                  Metadata = response['Metadata'],
+                  ContentType = contentType)
+
 def mark_processed(key):
     id = key.split('/')[-1] # ID is the part after the last '/'
     logger.info('Connecting to DB')
@@ -107,7 +121,7 @@ def mark_processed(key):
     cursor = conn.cursor()
     logger.info('Executing UPDATE')
     cursor.execute(
-        "UPDATE tros_metadatas SET ready = true WHERE id = %s", [id])
+        "UPDATE tros_metadatas SET ready = true, available_formats = ARRAY['full', 'original', 'thumbnail', 'aspect_thumbnail']::file_type[] WHERE id = %s", [id])
     logger.info('COMMITing')
     conn.commit()
     cursor.close()
